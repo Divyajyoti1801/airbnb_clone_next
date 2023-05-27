bed276d0c9176165e97d3e02e8772ae737849ec2
Divyajyoti <31211823+Divyajyoti1801@users.noreply.github.com>
2023-05-27 21:06:29 +0530

    dynamic server usage error solution : Trial 4

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,13 @@ import getListings, { IListingParams } from "./actions/getListings";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
-import { SafeListing } from "./types";
+
 interface HomeProps {
   searchParams: IListingParams;
 }
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+export const revalidate = 60;
 
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
@@ -18,7 +21,7 @@ const Home = async ({ searchParams }: HomeProps) => {
   return (
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {listings.map((listing: SafeListing) => {
+        {listings.map((listing: any) => {
           return (
             <ListingCard
               currentUser={currentUser}
@@ -34,4 +37,3 @@ const Home = async ({ searchParams }: HomeProps) => {
 
 export default Home;
 
-export const dynamic = "force-dynamic";
